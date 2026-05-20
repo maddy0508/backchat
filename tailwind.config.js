80b883a4f8f447c8fddbf4d7867255c0f08d0d30
maddy0508 <mckellarmaddy@gmail.com>
2026-05-20 22:34:22 +1000

    Add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        glow: '0 0 40px rgba(99, 102, 241, 0.18)'
+      }
+    }
+  },
+  plugins: []
+};
